8bbd0fc079e239352fb55ad7824342cb49c9a120
Mathias Maisberger <hiasinho@me.com>
2016-02-15 09:19:31 +0100

    New proteus-middleman project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: __dirname + "/_js",
+  entry: "./entry",
+  output: {
+    path: __dirname + "/js",
+    filename: "bundle.js"
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ],
+
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx', '.scss', '.css', 'config.js'],
+  },
+
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: "json" },
+      { test: /\.css$/, loader: "style!css" },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|_vendor)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "./node_modules/foundation-sites/scss/")]
+  }
+}
